bed6a0d76a1ed9699ac6daf418119c1a6d83ba15
Vitalii Vlkhivskyi <planetskaro7@gmail.com>
2022-09-20 13:17:50 +0300

    initialize

---
+++ dnn-utils.py
@@ -0,0 +1,30 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3.9.12 ('base')",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "name": "python",
+   "version": "3.9.12"
+  },
+  "orig_nbformat": 4,
+  "vscode": {
+   "interpreter": {
+    "hash": "060d923b49e0239bbc366407d0b9a70fa0635340106b5274efadddcb0cfd84a6"
+   }
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
